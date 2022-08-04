67a622b3fe2bd672d09e23c6fbf102625fd1cd3d
rivery <rivery.wang@vesoft.com>
2022-08-04 11:11:57 +0800

    add magicapi

---
+++ magicapi/src/test/java/com/vesoft/magicapi/MagicapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vesoft.magicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
