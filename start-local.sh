6801056d2169b859e8680b6f31e2ae9a19a43106
mizy <zhuang.miao@vesoft.com>
2023-01-31 06:17:03 +0000

    fix: database name

--- start-local.sh
+++ start-local.sh
@@ -3,7 +3,7 @@ export MYSQL_CN_USERNAME=root
 export MYSQL_CN_PASSWORD=nebula
 export MYSQL_CN_TZ=Asia/Shanghai
 
-export MYSQL_EN_URL=jdbc:mysql://192.168.8.157:3306/website
+export MYSQL_EN_URL=jdbc:mysql://192.168.8.157:3306/website_en
 export MYSQL_EN_USERNAME=root
 export MYSQL_EN_PASSWORD=nebula
 export MYSQL_EN_TZ=America/New_York
